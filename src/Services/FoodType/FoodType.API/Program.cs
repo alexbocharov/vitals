3bbbf583e5e49d7a946c0428c904aecc1a1a58b2
Alexander Bocharov <alex.bocharov@outlook.com>
2023-09-20 16:51:11 +0300

    chore: add health check endpoint

--- src/Services/FoodType/FoodType.API/Program.cs
+++ src/Services/FoodType/FoodType.API/Program.cs
@@ -1,9 +1,17 @@
 // Copyright (c) Alexander Bocharov. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapHealthChecks("/health", new HealthCheckOptions() { Predicate = _ => true });
+app.MapHealthChecks("/liveness", new HealthCheckOptions() { Predicate = r => r.Name.Contains("self") });
+
+app.MapGet("/", () => "Vitals FoodType API");
 
 app.Run();
